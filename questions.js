4eef7a131aac85b7b3edc48c037b4a93b707f3c1
Worldpanel <vananh8695@gmail.com>
2026-02-26 06:15:45 +0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,7 +5,8 @@ window.QUESTION_BANK = [
     text: `
 All cycles are slow.
 Motorbikes are not cycles.
-So, motorbikes are not slow.`,
+So, motorbikes are not slow.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -14,10 +15,11 @@ So, motorbikes are not slow.`,
 
   {
     key: "q3_2",
-    text: `(2)<br><br>
-All sports loving persons are athlete.<br><br>
-No Martians are athlete.<br><br>
-So, no Martians are sports loving.`,
+    text: `
+All sports loving persons are athlete.
+No Martians are athlete.
+So, no Martians are sports loving.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -26,10 +28,11 @@ So, no Martians are sports loving.`,
 
   {
     key: "q3_3",
-    text: `(3)<br><br>
-All young men like having fun.<br><br>
-All young men study in College.<br><br>
-So, all studying in College like having fun.`,
+    text: `
+All young men like having fun.
+All young men study in College.
+So, all studying in College like having fun.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -38,10 +41,11 @@ So, all studying in College like having fun.`,
 
   {
     key: "q3_4",
-    text: `(4)<br><br>
-All morons are imbeciles.<br><br>
-Most humans are not imbeciles.<br><br>
-So most humans are not morons.`,
+    text: `
+All morons are imbeciles.
+Most humans are not imbeciles.
+So most humans are not morons.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -50,10 +54,11 @@ So most humans are not morons.`,
 
   {
     key: "q3_5",
-    text: `(5)<br><br>
-All girls like ornaments.<br><br>
-Some actresses like ornaments.<br><br>
-So, some actresses are girls.`,
+    text: `
+All girls like ornaments.
+Some actresses like ornaments.
+So, some actresses are girls.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -62,10 +67,11 @@ So, some actresses are girls.`,
 
   {
     key: "q3_6",
-    text: `(6)<br><br>
-Artists are creative,<br><br>
-Most farmers are not creative.<br><br>
-So, most farmers are not artists.`,
+    text: `
+Artists are creative,
+Most farmers are not creative.
+So, most farmers are not artists.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -74,10 +80,11 @@ So, most farmers are not artists.`,
 
   {
     key: "q3_7",
-    text: `(7)<br><br>
-Hardworking people never succeed in life.<br><br>
-The Chinese are hardworking.<br><br>
-So, the Chinese never succeed in life.`,
+    text: `
+Hardworking people never succeed in life.
+The Chinese are hardworking.
+So, the Chinese never succeed in life.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -86,10 +93,11 @@ So, the Chinese never succeed in life.`,
 
   {
     key: "q3_8",
-    text: `(8)<br><br>
-All bullies are cowards.<br><br>
-No brave men are cowards.<br><br>
-So, no brave men are bullies.`,
+    text: `
+All bullies are cowards.
+No brave men are cowards.
+So, no brave men are bullies.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -98,10 +106,11 @@ So, no brave men are bullies.`,
 
   {
     key: "q3_9",
-    text: `(9)<br><br>
-All Officers are Executives.<br><br>
-Supervisors are not Officers.<br><br>
-So, Supervisors are not Executives.`,
+    text: `
+All Officers are Executives.
+Supervisors are not Officers.
+So, Supervisors are not Executives.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
@@ -110,13 +119,15 @@ So, Supervisors are not Executives.`,
 
   {
     key: "q3_10",
-    text: `(10)<br><br>
-All crustaceans live in shells.<br><br>
-Many crabs live in shells.<br><br>
-So, many crabs are crustaceans.`,
+    text: `
+All crustaceans live in shells.
+Many crabs live in shells.
+So, many crabs are crustaceans.
+`,
     options: [
       { value: "TRUE", label: "TRUE" },
       { value: "FALSE", label: "FALSE" }
     ]
   }
+
 ]; // END OF QUESTION BANK
